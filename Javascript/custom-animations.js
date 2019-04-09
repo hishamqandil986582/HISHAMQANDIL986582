68e8c7436ff0a23b03099c01204868552d8aaf64
hishamqandil986582 <49167662+hishamqandil986582@users.noreply.github.com>
2019-04-09 08:14:43 +0300

    IMAGE

---
+++ Javascript/custom-animations.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// include the custom animation
+let MY_CUSTOM = "" +
+    "               |----\n"+
+    "   L*L========[_]L)\n"+
+    "             -'-`-\n"+
+    "=====\n" +
+    "      L   -----|\n"+
+    "    * ========[_]L)\n"+
+    "   L         -'-`-\n"+
+    "=====\n" +
+    "    L     -----|\n"+
+    "    * ========[_]L)\n"+
+    "    L        -'-`-\n"+
+    "=====\n" +
+    "   L           |----\n"+
+    "    * ========[_]L)\n"+
+    "     L       -'-`-\n";
+
+ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = MY_CUSTOM;
